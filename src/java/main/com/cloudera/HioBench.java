2655e0fa07d4803af440bea3c14ba1d2ac3c2c86
Colin Patrick Mccabe <cmccabe@cloudera.com>
2013-01-24 15:56:44 -0800

    remove mapred includes
    Signed-off-by: Colin Patrick McCabe <cmccabe@cloudera.com>

--- src/java/main/com/cloudera/HioBench.java
+++ src/java/main/com/cloudera/HioBench.java
@@ -47,15 +47,6 @@ import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
-import org.apache.hadoop.mapred.FileInputFormat;
-import org.apache.hadoop.mapred.FileOutputFormat;
-import org.apache.hadoop.mapred.JobClient;
-import org.apache.hadoop.mapred.JobConf;
-import org.apache.hadoop.mapred.MapReduceBase;
-import org.apache.hadoop.mapred.Mapper;
-import org.apache.hadoop.mapred.OutputCollector;
-import org.apache.hadoop.mapred.Reducer;
-import org.apache.hadoop.mapred.Reporter;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
 
